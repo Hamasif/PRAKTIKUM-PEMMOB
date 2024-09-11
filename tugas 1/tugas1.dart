de80892e1e86a977853595a8e99753db7c531429
Hamasif <150764266+Hamasif@users.noreply.github.com>
2024-09-11 18:32:17 +0700

    Add files via upload

--- tugas 1/tugas1.dart
+++ tugas 1/tugas1.dart
@@ -1,16 +1,14 @@
-import 'dart:io';
 void main() {
-  int angka = 5; // Angka yang akan dihitung faktorialnya
-  int hasil = faktorial(angka); // Menghitung faktorial
+  int angka = 5; 
+  int hasil = faktorial(angka); 
   
   print('Faktorial dari $angka adalah: $hasil');
 }
 
-// Fungsi untuk menghitung faktorial
 int faktorial(int n) {
   if (n <= 1) {
-    return 1; // Basis rekursi
+    return 1; 
   } else {
-    return n * faktorial(n - 1); // Langkah rekursi
+    return n * faktorial(n - 1); 
   }
 }
